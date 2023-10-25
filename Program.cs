69d4b923a278fb5bacd7da7346c02273940a4f49
Adr1anL <146196415+Adr1anL@users.noreply.github.com>
2023-10-25 08:42:02 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace varijable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 100;
+            int b = 3;
+
+            Console.WriteLine("{0} dijeljeno s {1} je {2} i ostatak {3}", a, b, a / b, a % b);
+
+            Console.ReadKey();
+        }
+    }
+}
